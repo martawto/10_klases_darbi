69d21f699069cff5ec76c30219883f51f12412ed
martawto <marta.vainina@je.pvg.edu.lv>
2023-01-15 20:49:14 +0200

    g

--- 2_uzdevums.py
+++ 2_uzdevums.py
@@ -1,12 +1,15 @@
 #Atrast skaitļa n faktoriālu, ja n vērtību ievada no klaviatūras
 #n! = 1*2*3* ... *n
+
+
 skaitlisn=int(input("Ievadi n vērtību:"))
 
 faktorials=1
-n=0
 i=1
+
 while i <= skaitlisn:
-    n=faktorials*i
-    i+=1
-    print(n)
+    faktorials=faktorials * i
+    i=i + 1
+
+print("Faktoriāls no", skaitlisn, " ir ", faktorials)
     

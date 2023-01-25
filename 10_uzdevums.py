192bf6a33bd2c0bc2f0b45508a801671d22333da
martawto <marta.vainina@je.pvg.edu.lv>
2023-01-25 13:47:00 +0200

    h

---
+++ 10_uzdevums.py
@@ -0,0 +1,24 @@
+#y = 2x^2, x pieder[-5;5], solis 0,5;
+
+#i=1
+
+#for x in range(-5, 5):
+  #  i = x+0.5
+   # print(i, end="; ")
+
+solis= 0.5
+bv = 5
+x= -5
+
+print("x | y")
+print("-----")
+
+while x <= bv:
+    print(x, " | ", 2 * x**2)
+    x += solis
+
+
+
+    
+
+

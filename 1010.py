852350099266b366bf4b5a9449312830415eb500
martawto <marta.vainina@je.pvg.edu.lv>
2023-02-27 13:30:38 +0200

    t

---
+++ 1010.py
@@ -0,0 +1,6 @@
+#Sastādīt programmu, kas ģenerē nejaušu skaitļu masīvu ar N (ne vairāk par 50) naturāliem
+#skaitļiem (ne lielākiem par 1000), saglabā tos masīvā, aprēķina un izvada uz ekrāna:
+#a) visus masīva elementus,
+#b) visu skaitļu vidējo aritmētisko vērtību,
+#c) masīva lielāko skaitli un masīva mazāko skaitli,
+#d) pāra un nepāra skaitļu skaitu.
---
+++ 5_6_uzd_summa.py
@@ -0,0 +1,4 @@
+#Uzdevuma izpildē izmantot funkciju skaitļa faktoriāla (k!) aprēķināšanai. 
+#Uzrakstīt programmu 2 veidos:
+#1 ar for cikla pielietojumu
+#2. ar while cikla pielietojumu

d67446afca824732da013eedbb689b961b805029
martawto <marta.vainina@je.pvg.edu.lv>
2023-04-17 13:30:10 +0300

    g

--- 2_dimension/12_04_uzd.py
+++ 2_dimension/12_04_uzd.py
@@ -12,10 +12,33 @@
 # Visu pārējo funkcionalitāti katrs var papildināt pats.
 
 import random
-skaitli = []
-n = int(input("Cik nejaušus skaitļus ģenerēsi? "))
+apjoms = 0
+# skaitli = []
+# n = int(input("Cik nejaušus skaitļus ģenerēsi? "))
 
-for i in range(n): skaitli.append(random.randint(0, 99))
-print(skaitli)
+# for i in range(n): skaitli.append(random.randint(0, 99))
+# print(skaitli)
 
-darbsk = int(input("Kādu darbību izmantosi, '+' '-''/''**' ?"))
+# darbsk = int(input("Kādu darbību izmantosi, '+' '-''/''**' ?"))
+
+cik = int(input("Cik uzdevumus vēlies risināt?"))
+
+while apjoms < 1 or apjoms > 2:
+    print("Kādā apjomā vēlaties risināt? Ievadi 1 ja no 0-9 vai 2 ja no 0-20")
+    apjoms = int(input("1 vai 2: "))
+    if apjoms == 1:
+        apjoms = 9
+    if apjoms == 2:
+        apjoms ==20
+
+for i in range(cik):
+    a= random.randint(0,apjoms)
+    b = random.randint(0,apjoms)
+
+    print("Cik ir", a, "*", b, "=")
+    atb = int(input("Atbilde: "))
+
+    if a* b == atb:
+        print("Pareizi!")
+    else:
+        print("Nepareizi! Pareizā atbilde ir", a * b)
